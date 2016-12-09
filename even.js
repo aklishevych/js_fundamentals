b9a5c689771fed6d6965fd9e1fe6cd2fb8ebcd18
aklishevych <omakase.anna@gmail.com>
2016-12-09 01:43:38 -0500

    Adds functions exercise

---
+++ even.js
@@ -0,0 +1,15 @@
+function isEven(num) {
+  return num % 2 === 0
+}
+var tenIsEven = isEven(10);
+var elevIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevIsEven);
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
---
+++ hello.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Anna");
+sayHello("Raz")
+function sayHello() {
+  console.log("Hello, world")
+}
+sayHello();
+function sayHello(name) {
+  console.log("Hello, " + name);
+
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
